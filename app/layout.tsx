b5626376a5e478726c80351268740b8af74a6761
Abhishek Katnoria <abhishekkatnoria993@gmail.com>
2024-06-02 17:31:23 +0530

    Setup The project

---
+++ app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import { Inter, IBM_Plex_Serif } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
+const ibmPlexserif = IBM_Plex_Serif({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-ibm-Plex-serif",
+});
+
+export const metadata = {
+  title: "Bank of America",
+  description: "Welcome to Bank of America",
+  icons: {
+    icon: "./icons/logo.svg",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${inter.variable} ${ibmPlexserif.variable}`}>
+        {children}
+      </body>
+    </html>
+  );
+}
